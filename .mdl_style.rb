66a43da3e7fff97ef6f44c940b5dfb6f37773b22
Cutler, Philip <10864839+greenthegarden@users.noreply.github.com>
2025-02-22 14:28:29 +1030

    feat: add icons to README

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,4 +1,4 @@
 all
 exclude_rule 'MD007' # Unordered list indentation
-rule 'MD013', :line_length => 250, :tables => false, :ignore_code_blocks => true
+rule 'MD013', :line_length => 400, :tables => false, :ignore_code_blocks => true
 # exclude_rule 'MD041'
