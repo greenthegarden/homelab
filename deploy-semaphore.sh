bf10c5ab5de37061e90d71a62f0a4433794b4a1c
Cutler, Philip <10864839+greenthegarden@users.noreply.github.com>
2025-08-13 09:02:27 +0930

    fix: ensure docker module is install

--- deploy-semaphore.sh
+++ deploy-semaphore.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+ansible-galaxy collection install community.docker
+
 ansible-playbook --inventory-file inventory-homelab_lxc.yaml --vault-password-file vault.pass playbooks/deploy-semaphore.yaml
